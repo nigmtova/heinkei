9918aa714151e573ebdeab2ae78c4627aa37b908
nigmtova <144693420+nigmtova@users.noreply.github.com>
2024-09-15 21:31:03 +0500

    aaa
    aaa

---
+++ _next/static/chunks/pages/[...slug]-118c5d0dee942f2f-4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[330],{881:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[...slug]",function(){return _(6944)}])},6944:function(n,u,_){"use strict";_.r(u);var t=_(8662);u.default=t.Z}},function(n){n.O(0,[28,495,639,774,888,179],function(){return n(n.s=881)}),_N_E=n.O()}]);
